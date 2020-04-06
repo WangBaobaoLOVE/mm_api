21d26eaf7fe4d30d6ad307474c41e3f76c86cd25
WangBaobaoLOVE <wbb392797665@sina.com>
2020-04-06 10:31:09 +0800

    update

--- README_zh.md
+++ README_zh.md
@@ -19,7 +19,7 @@
 
 但是，我们建议您使用docker来部署服务器，具体要求如下：
 
--docker，版本18.09.7+
+- docker，版本18.09.7+
 
 注意：以上部署环境已经过测试，但是我们不能保证其他环境是否可以正常运行。
 
@@ -51,7 +51,7 @@ cd mm_phm
 sudo docker build --tag mm_phm:1.0 .
 ```
 
-分三步建立应用镜像的原因是因为CentOS基本镜像的拉取和python3的安装比较耗时，并且第三方依赖以后可能也会增加。最重要的是便于快速发展。
+分三步建立应用镜像的原因是因为CentOS基本镜像的拉取和python3的安装比较耗时，并且第三方依赖以后可能也会增加。最重要的是便于快速开发。
 
 ## 启动
 
@@ -60,7 +60,7 @@ sudo docker build --tag mm_phm:1.0 .
 sudo docker run -it -p 8000:8000 --name test1 mm_phm:1.0
 ```
 
-#### 模式 2: 后台 (推荐模式)
+#### 模式 2: 后台模式(推荐模式)
 ```
 sudo docker run -d -p 8000:8000 --name test1 mm_phm:1.0
 ```
@@ -72,12 +72,58 @@ sudo docker run -d -p 8000:8000 --name test1 mm_phm:1.0
 默认情况下，服务将在8000端口上运行HTTP服务, 并使用RESTful API：
 
 ```
-http://127.0.0.1:8000/apiv3/opendata/analyze/pumpcheck，Method= ['GET'，'POST']
+http://127.0.0.1:8000/apiv3/opendata/analyze/*****，Methods= ['GET'，'POST']
 ```
 
 对于请求，如果方法为`GET`，则服务将返回API文档（`HTML`格式）。
 
 对于请求，如果方法是`POST`，则服务将在分析数据后返回结果（`json`格式）。当然，您首先需要在请求`body`中插入`json`格式的机械设备数据
+
+**目前PHM服务所提供的API主要分为三个部分：**
+
+1. 水泵的异常点判断（三次多项式线性拟合）
+2. 设备的故障点判断（随机森林分类）
+3. 设备的剩余使用寿命（RUL）预测（LSTM回归）
+
+**您可以通过如下方法来或获知所提供的API及其调用方法:**
+
+```
+http://127.0.0.1:8000/apiv3/opendata/analyze/
+```
+
+```
+http://127.0.0.1:8000/apiv3/opendata/analyze/index
+```
+
+**以设备的剩余使用寿命（RUL）预测（LSTM回归)的API规范为例说明:**
+
+- **`GET`方法获得API说明文档**
+
+```
+http://127.0.0.1:8000/apiv3/opendata/analyze/machineRULtrain/
+```
+
+- **`POST` 方法获得建模结果**
+
+```
+http://127.0.0.1:8000/apiv3/opendata/analyze/machineRULtrain/
+```
+在`request`的`body`提交的`json`格式文件举例如下:
+
+```
+{
+	"temperature":[31,36,37,27],
+	"power":[891,784,0,1025],
+	"label":[0,0,1,0]
+}
+```
+上面的数据中，代表第一个时刻设备的温度为31，功率为891，0代表当前时刻机器的剩余寿命为0个机器周期，第二个时刻设备的温度为36，功率为784，当前时刻机器的剩余寿命为0个机器周期，以此类推。
+
+**Note :***该API是建立设备故障模型,并不是利用模型分析数据,所以其不返回建模结果,而是返回`布尔类型`表明是否建模成功*.`True`表示建模成功,其保存模型的`h5`文件会保存在容器中;`False`表示建模失败
+
+```
+True or False
+```
 
 ##其他信息
 
--- mm_phm/app.py
+++ mm_phm/app.py
@@ -1,10 +1,27 @@
 import json
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.layers import Dense, Dropout, LSTM
+from tensorflow.python.keras.models import Sequential, load_model
+import os
+import h5py     #这个包以及上面提到的包都需要安装！！！
+
 from flask import Flask,jsonify,request,render_template,send_file
 
 app = Flask(__name__)
 
-@app.route('/apiv3/opendata/analyze/pumpcheck' ,methods=['GET', 'POST'])
+
+@app.route('/apiv3/opendata/analyze/',methods=['GET'])
+def index1():
+    return send_file('help_docs/index.html')
+
+@app.route('/apiv3/opendata/analyze/index/' ,methods=['GET'])
+def index2():
+    return send_file('help_docs/index.html')
+
+@app.route('/apiv3/opendata/analyze/pumpcheck/' ,methods=['GET', 'POST'])
 def pumpcheck():
     if request.method == 'GET':
         return send_file('help_docs/help_pumpcheck.html')
@@ -79,6 +96,234 @@ def pumpcheck():
     json.dump(result, f)
     # f.close()
     return jsonify(result)
+
+
+def machinechecktrain(path1="machinedata.json", savepath='''rfmodel.m'''):
+
+    with open(path1, mode='r', encoding='UTF-8') as f:
+        Data1 = json.load(f)  # 将数据读入Data
+
+    dim = len(Data1)  # 获取数据的维度
+    key_list = Data1.keys()
+    temp = []
+    for i in range(dim - 1):
+        temp.append(Data1.get(list(key_list)[i]))
+    label = Data1.get(list(key_list)[dim - 1])
+    SampleNum = len(temp[0])
+    DataArray = np.mat(np.zeros((SampleNum, dim - 1)))  # 生成一个0矩阵
+    for i in range(dim - 1):
+        for j in range(SampleNum):
+            DataArray[j, i] = temp[i][j]
+
+    for i in range(dim - 1):
+        sum = 0
+        mse = 0
+        t = 0
+        for j in range(SampleNum):
+            if DataArray[j, i] != 0:
+                sum = sum + DataArray[j, i]
+                t = t + 1
+        aver = sum / t
+        for j in range(SampleNum):
+            if DataArray[j, i] == 0:
+                DataArray[j, i] = aver
+        for j in range(SampleNum):
+            mse = mse + (DataArray[j, i] - aver) ** 2
+        mse = (mse / (t - 1)) ** 0.5
+        for j in range(SampleNum):
+            DataArray[j, i] = (DataArray[j, i] - aver) / mse
+
+    model = RandomForestClassifier(n_estimators=50,
+                                   bootstrap=True,
+                                   max_depth=None,
+                                   min_samples_leaf=1
+                                   )
+    model.fit(DataArray, label)
+    joblib.dump(model, savepath)
+
+
+def machinechecktest(path2="machinetest.json", modelpath='rfmodel.m', savepath="resultm.json"):
+
+    with open(path2, mode='r', encoding='UTF-8') as f:
+        Data2 = json.load(f)
+
+    dim = len(Data2)+1  # 获取数据的维度
+    key_list = Data2.keys()
+    temp = []
+    for i in range(dim - 1):
+        temp.append(Data2.get(list(key_list)[i]))
+    SampleNum = len(temp[0])
+
+
+    tempT = []
+    for i in range(dim - 1):
+        tempT.append(Data2.get(list(key_list)[i]))
+    SampleNumT = len(tempT[0])
+    DataArrayT = np.mat(np.zeros((SampleNumT, dim - 1)))  # 生成一个0矩阵
+    for i in range(dim - 1):
+        for j in range(SampleNumT):
+            DataArrayT[j, i] = tempT[i][j]
+
+    for i in range(dim - 1):
+        sum = 0
+        mse = 0
+        t = 0
+        for j in range(SampleNumT):
+            if DataArrayT[j, i] != 0:
+                sum = sum + DataArrayT[j, i]
+                t = t + 1
+        aver = sum / t
+        for j in range(SampleNumT):
+            if DataArrayT[j, i] == 0:
+                DataArrayT[j, i] = aver
+        for j in range(SampleNumT):
+            mse = mse + (DataArrayT[j, i] - aver) ** 2
+        mse = (mse / (t - 1)) ** 0.5
+        for j in range(SampleNumT):
+            DataArrayT[j, i] = (DataArrayT[j, i] - aver) / mse
+
+    model = joblib.load(modelpath)
+    ypred = model.predict(DataArrayT)
+
+    result = []
+    for i in range(len(ypred)):
+        result.append(float(ypred[i]))
+
+    filename = savepath
+    f = open(filename, 'w')
+    json.dump(result, f)
+    f.close()
+    return True
+
+
+
+@app.route('/apiv3/opendata/analyze/machineRULtrain/' ,methods=['GET', 'POST'])
+def machineRULtrain(savepath='lstmmodel.h5'):
+    if request.method == 'GET':
+        return send_file('help_docs/machineRULtrain.html')
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    #with open(path1, mode='r', encoding='UTF-8') as f:
+        #Data1 = json.load(f)  # 将数据读入Data
+    Data1 = request.get_json()
+    dim = len(Data1)  # 获取数据的维度
+    key_list = Data1.keys()
+    temp = []
+    for i in range(dim - 1):
+        temp.append(Data1.get(list(key_list)[i]))
+    label = Data1.get(list(key_list)[dim - 1])
+    SampleNum = len(temp[0])
+    DataArray = np.mat(np.zeros((SampleNum, dim - 1)))  # 生成一个0矩阵
+    for i in range(dim - 1):
+        for j in range(SampleNum):
+            DataArray[j, i] = temp[i][j]
+
+    for i in range(dim - 1):
+        sum = 0
+        mse = 0
+        t = 0
+        for j in range(SampleNum):
+            if DataArray[j, i] != 0:
+                sum = sum + DataArray[j, i]
+                t = t + 1
+        aver = sum / t
+        for j in range(SampleNum):
+            if DataArray[j, i] == 0:
+                DataArray[j, i] = aver
+        for j in range(SampleNum):
+            mse = mse + (DataArray[j, i] - aver) ** 2
+        mse = (mse / (t - 1)) ** 0.5
+        for j in range(SampleNum):
+            DataArray[j, i] = (DataArray[j, i] - aver) / mse
+
+    TrainDataX = np.zeros((SampleNum, dim - 1))
+    TrainDataY = np.zeros((SampleNum, 1))
+    for i in range(SampleNum):
+        TrainDataX[i] = DataArray[i]
+    for i in range(SampleNum):
+        TrainDataY[i] = label[i]
+    TrainDataX = TrainDataX.reshape(SampleNum, 1, dim - 1)
+
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(1, dim - 1)))
+    model.add(Dense(30))
+    model.add(Dropout(0.3))
+    model.add(Dense(30))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    es = EarlyStopping(monitor='accuracy', patience=5)
+
+    model.fit(TrainDataX, TrainDataY,  # TrainDataX,TrainDataY一定要是ndarry
+              batch_size=512,
+              epochs=50,
+              callbacks=[es],
+              shuffle=False)
+
+    model.save(savepath)
+    return "True"
+
+
+def machineRULtest(path2="machinetest.json", modelpath='lstmmodel.h5', savepath="resultRUL.json"):
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    with open(path2, mode='r', encoding='UTF-8') as f:
+        Data2 = json.load(f)
+
+    dim = len(Data2)+1  # 获取数据的维度
+    key_list = Data2.keys()
+    temp = []
+    for i in range(dim - 1):
+        temp.append(Data2.get(list(key_list)[i]))
+    SampleNum = len(temp[0])
+
+    tempT = []
+    for i in range(dim - 1):
+        tempT.append(Data2.get(list(key_list)[i]))
+    SampleNumT = len(tempT[0])
+    DataArrayT = np.mat(np.zeros((SampleNumT, dim - 1)))  # 生成一个0矩阵
+    for i in range(dim - 1):
+        for j in range(SampleNumT):
+            DataArrayT[j, i] = tempT[i][j]
+
+    for i in range(dim - 1):
+        sum = 0
+        mse = 0
+        t = 0
+        for j in range(SampleNumT):
+            if DataArrayT[j, i] != 0:
+                sum = sum + DataArrayT[j, i]
+                t = t + 1
+        aver = sum / t
+        for j in range(SampleNumT):
+            if DataArrayT[j, i] == 0:
+                DataArrayT[j, i] = aver
+        for j in range(SampleNumT):
+            mse = mse + (DataArrayT[j, i] - aver) ** 2
+        mse = (mse / (t - 1)) ** 0.5
+        for j in range(SampleNumT):
+            DataArrayT[j, i] = (DataArrayT[j, i] - aver) / mse
+
+    TestDataX = np.zeros((SampleNum, dim - 1))
+
+    for i in range(SampleNum):
+        TestDataX[i] = DataArrayT[i]
+    TestDataX = TestDataX.reshape(SampleNum, 1, dim - 1)
+
+    model = load_model(modelpath)
+    ypred = model.predict(TestDataX)
+
+    result = []
+    for i in range(len(ypred)):
+        result.append(float(ypred[i]))
+
+    filename = savepath
+    f = open(filename, 'w')
+    json.dump(result, f)
+    f.close()
+    return True
 
 if __name__ == "__main__":
 	app.run(port=8000)
